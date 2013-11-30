968854b3404bf371e5046ffe6887fc548c65e7c0
Seth Helfgott <sethleland@gmail.com>
2013-11-29 20:30:24 -0500

    Seeds production database

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,5 @@
 class Note < ActiveRecord::Base
-  attr_accessible :user_id, :crop_id, :list_id, :title, :text, :note_photo, :due_date, :flag1, :flag2, :flag3, :flag4, :flag5, :assigned_user, :note_pos
-
+  attr_accessible :user_id, :crop_id, :list_id, :title, :text, :note_photo, :due_date, :flag1, :flag2, :flag3, :flag4, :flag5, :assigned_user, :note_pos, :note_photo_file_name, :note_photo_content_type, :note_photo_file_size, :note_photo_updated_at, :completion
   validates :user_id, :list_id, :title, presence: true
 
   belongs_to :user
